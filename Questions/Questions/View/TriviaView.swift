656e26039a20488ffd20d2abb9e16ba449c38aa5
ingwarrrr <igorek0706@gmail.com>
2022-10-28 10:19:06 +0300

    Result score added

--- Questions/Questions/View/TriviaView.swift
+++ Questions/Questions/View/TriviaView.swift
@@ -11,8 +11,31 @@ struct TriviaView: View {
     @EnvironmentObject var trivialManager: TriviaManager
 
     var body: some View {
-        QuestionView()
-            .environmentObject(trivialManager)
+        if trivialManager.reachedEnd {
+            VStack(spacing: 20) {
+                Text("Question Game")
+                    .lightBlueTitle()
+                
+                Text("Поздравляю, Вы прошли игру!")
+                
+                Text("Вы набрали \(trivialManager.score) из \(trivialManager.length)")
+                
+                Button {
+                    Task.init {
+                        await trivialManager.fetchTrivial()
+                    }
+                } label: {
+                    MainButton(text: "Новая игра")
+                }
+            }
+            .foregroundColor(.accentColor)
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(red: 0.5, green: 0.5, blue: 1))
+        } else {
+            QuestionView()
+                .environmentObject(trivialManager)
+        }
     }
 }
 
