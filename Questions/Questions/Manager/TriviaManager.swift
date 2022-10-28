60f9c2d899e2fb7509307c1b2c27891ab5ea9896
ingwarrrr <igorek0706@gmail.com>
2022-10-28 08:55:12 +0300

    TrivialManager core functional added

---
+++ Questions/Questions/Manager/TriviaManager.swift
@@ -0,0 +1,78 @@
+//
+//  TriviaManager.swift
+//  Questions
+//
+//  Created by Igor on 28.10.2022.
+//
+
+import Foundation
+import SwiftUI
+
+class TriviaManager: ObservableObject {
+    private(set) var trivia: [Trivia.Result] = []
+    @Published private(set) var length = 0
+    @Published private(set) var index = 0
+    @Published private(set) var reachedEnd = false
+    @Published private(set) var answerSelected = false
+    @Published private(set) var question: AttributedString = ""
+    @Published private(set) var answerChoices: [Answer] = []
+    @Published private(set) var progress: CGFloat = 0.00
+    @Published private(set) var score = 0
+
+    init() {
+        Task.init {
+            await fetchTrivial()
+        }
+    }
+    
+    func fetchTrivial() async {
+        guard let url = URL(string: "https://opentdb.com/api.php?amount=10") else { fatalError("Ошибка Url") }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Ошибка при получении данных") }
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let decodedData = try decoder.decode(Trivia.self, from: data)
+            
+            DispatchQueue.main.async {
+                self.trivia = decodedData.results
+                self.length = self.trivia.count
+                self.setQuestion()
+            }
+        } catch {
+            print("Ошибка получения trivia: \(error.localizedDescription)")
+        }
+    }
+    
+    func goToNextQuestion() {
+        if index + 1 < length {
+            index += 1
+            setQuestion()
+        } else {
+            reachedEnd = true
+        }
+    }
+    
+    func setQuestion() {
+        answerSelected = false
+        progress = CGFloat((index + 1) / length * 350)
+        
+        if index < length {
+            let currentQuestion = trivia[index]
+            question = currentQuestion.formattedQuestion
+            answerChoices = currentQuestion.answers
+        }
+    }
+    
+    func selectAnswer(answer: Answer) {
+        answerSelected = true
+        if answer.isCorrect {
+            score += 1
+        }
+    }
+}
